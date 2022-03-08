93e5697d2e10ff11f9ed50aa68a4c88f94fa8aa2
Davi Luna <davijluna@gmail.com>
2022-03-08 17:47:30 -0300

    requesito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => { };
-
-module.exports =  average;
+const average = (array) => {
+  let media = 0;
+  if (array.length  === 0) {
+    return undefined;
+  }
+    for (let index = 0; index < array.length; index += 1) {
+     if (typeof array[index] === 'string') {
+       return undefined;
+     }
+    media += array[index];
+    }
+    media = Math.round(media /= array.length); 
+ return media;
+}
+module.exports = average;
